06e6debb4ec84f50d8dd7c0fa780ecfdb47ca737
megautam-git <gautamsingh68@gmail.com>
2018-11-17 00:07:54 +0530

    applied list

--- Interview.java
+++ Interview.java
@@ -2,16 +2,19 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.*;
 
 public class Interview{
     public static String arrangeSentence(String text){
         String text1="";
        String[] arr=text.split(" ");
-       Set<String> set=new HashSet<>();
+       //Set<String> set=new HashSet<>();
+       List<String> list=new ArrayList<>();
        for (int i = 0; i < arr.length; i++) {
-           set.add(arr[i]);
+           list.add(arr[i]);
        }
-       Iterator it=set.iterator();
+       Collections.sort(list);
+       Iterator it=list.iterator();
        while(it.hasNext()){
          text1+=it.next()+" ";
        }
